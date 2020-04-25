d0749292893992ddf3fa780f50a20717b2b438bd
wwsalmon <wwsamson@yahoo.com>
2020-04-25 16:03:38 -0400

    make margins on pieChart work

--- 2019/index.js
+++ 2019/index.js
@@ -3,7 +3,15 @@ define(['barChart','pieChart','masonry'], function(barChart, pieChart, masonry){
     // D3/sota.js
 
     barChart({ selector: "#module-general-ethnicity-d3", dataFile: "ethnicity", totalResp: 1052, maxVal: "maxVal" });
-    pieChart({ selector: "#module-general-community-d3", dataFile: "community"});
+
+    margin = {
+        "top": 100,
+        "bottom": 0,
+        "left": 200,
+        "right": 100
+    }
+
+    pieChart({ selector: "#module-general-community-d3", dataFile: "community", margin: margin});
 
     // MASONRY LAYOUT
 
--- js/components/pieChart.js
+++ js/components/pieChart.js
@@ -12,14 +12,27 @@ define(['d3', 'helper'], function (d3, helper) {
         separatorStroke = 8,
         margin = {
             "top": 0,
-            "bottom": 0
+            "bottom": 0,
+            "left": 0,
+            "right": 0
         } }) {
 
         var svg = d3.select(selector).append("svg");
 
         var width = document.querySelector(selector).offsetWidth;
+        var trueWidth = width - margin.left - margin.right;
 
-        var height = width + margin.top + margin.bottom;
+        if (trueWidth < pieRad * 2) {
+            pieRad = trueWidth / 2;
+        }
+
+        if (pieThick > pieRad){
+            pieThick = 50;
+        }
+
+        console.log(margin);
+
+        var height = pieRad * 2 + margin.top + margin.bottom;
 
         svg.attr("height", height);
 
@@ -37,14 +50,14 @@ define(['d3', 'helper'], function (d3, helper) {
                 var totalResp = values.reduce((a, b) => +a + +b, 0);
                 var percentages = values.map(value => helper.oneDecimal(100 * value / totalResp));
             }
-            else{
+            else {
                 var percentages = data.map(d => d.value);
             }
 
             // centered g to place chart in
 
             var g = svg.append("g")
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+                .attr("transform", "translate(" + (trueWidth / 2 + margin.left) + "," + (pieRad + margin.top) + ")");
 
             var pie = d3.pie();
             var pieData = pie(percentages);
