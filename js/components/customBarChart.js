5a6fa6c7733370d3940ab5c4162ae6110915941c
wwsalmon <wwsamson@yahoo.com>
2020-04-29 16:51:44 -0400

    fix up customBarChart SVG import to only import path and make it selectable

--- js/components/customBarChart.js
+++ js/components/customBarChart.js
@@ -4,7 +4,7 @@ export default function ({
     selector,
     dataFile,
     shapeFile,
-    shapeWidth = 200,
+    shapeWidth = 300,
     inputIsPercentage = false,
     height = 300,
     margin = {
@@ -24,23 +24,50 @@ export default function ({
     svg.attr("height", height);
 
     d3.xml("shapes/" + shapeFile + ".svg").then(shape => {
+
+        // import shape and make it a definition
+
         var importedNode = document.importNode(shape.documentElement, true)
-        
-        svg.append("defs")
-            .append("svg")
-            .attr("id","shape-def")
-            .attr("x",0)
-            .attr("y",0)
-            .attr("width", shapeWidth)
-            .append(() => importedNode.cloneNode(true));
 
-        var shapeContainer = svg.append("g")
-            .attr("transform", `translate(${(width - shapeWidth) / 2} 0)`);
+        var firstPath = d3.select(importedNode)
+            .select("path")
 
-        shapeContainer.append("use")
-            .attr("xlink:href", "#shape-def")
+        console.log(firstPath);
 
-        console.log(shape.select("#shape-def"));
+        let shapeCurrentWidth = 0;
+
+        var shapeContainer = svg.append("g");
+
+        shapeContainer.append(() => firstPath._groups[0][0])
+            .attr("class", "sota-shapePath")
+            .attr("transform",function(){
+                return `scale(${shapeWidth / this.getBBox().width})`
+            })
+
+        d3.csv("data/" + dataFile + ".csv").then(data => {
+
+            // process data
+
+            if (!inputIsPercentage) {
+                var values = data.map(d => d.value);
+                var totalResp = d3.sum(values, d => d);
+                var percentages = values.map(value => 100 * value / totalResp);
+            }
+            else {
+                var percentages = data.map(d => d.value);
+            }
+
+            // configure labelset
+
+            if (inputIsPercentage) {
+                var labelset = d3.map(percentages, d => d3.format(".1f")(d) + "%");
+            }
+            else {
+                var labelset = values;
+            }
+
+        });
+
     })
 
     d3.csv("data/" + dataFile + ".csv").then(data => {
@@ -51,27 +78,9 @@ export default function ({
 
         var labels = data.map(d => d.label);
 
-        if (!inputIsPercentage) {
-            var values = data.map(d => d.value);
-            var totalResp = d3.sum(values, d => d);
-            var percentages = values.map(value => 100 * value / totalResp);
-        }
-        else {
-            var percentages = data.map(d => d.value);
-        }
-
         // process data here. Create scales, etc.
 
         // LABELSET for tooltip:
-
-        if (inputIsPercentage) {
-            var labelset = percentages;
-            var tooltipAppend = "%";
-        }
-        else {
-            var labelset = values;
-            var tooltipAppend = "";
-        }
 
         // run main loop here
 
