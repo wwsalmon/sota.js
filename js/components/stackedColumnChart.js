e18515cbd4dd8a52cb8e09df42ad2d1a7618ee8d
createandbuild <33373263+createandbuild@users.noreply.github.com>
2020-05-03 11:29:13 -0400

    added stacked column and testing branch

---
+++ js/components/stackedColumnChart.js
@@ -0,0 +1,45 @@
+import { sotaConfig } from '../helper.js';
+
+export default function ({
+                             selector,
+                             dataFile,
+                             inputIsPercentage = false,
+                             margin = sotaConfig.margin
+                         }) {
+
+    // define styling variables here
+
+    const hoverOpacity = 0.8;
+    const overflowOffset = sotaConfig.overflowOffset;
+
+    const container = d3.select(selector);
+    const svg = container.append("svg");
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip");
+
+    const mainChart = svg.append("g")
+        .attr("class", "sota-mainChart");
+
+    const width = document.querySelector(selector).offsetWidth;
+    const mainWidth = width - margin.left - margin.right;
+
+    d3.csv("data/" + dataFile + ".csv").then(data => {
+
+        // data processing
+
+        // loop through to render stuff
+
+        // set widths, heights, offsets
+
+        let mainHeight = 0; // define mainHeight and height at some point
+        let height = mainHeight + margin.top + margin.bottom;
+
+        svg.style("width", width + 2 * overflowOffset + "px")
+            .attr("height", height)
+            .attr("transform", `translate(${-overflowOffset} 0)`);
+
+        mainChart.attr("transform",`translate(${margin.left} ${margin.top})`)
+            .attr("width",mainWidth)
+
+    });
+}
