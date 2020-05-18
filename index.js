4ed14ed37ae8000afee16994f6ed542867d99c07
wwsalmon <wwsamson@yahoo.com>
2020-05-18 00:06:01 -0400

    set colors, styles through js instead of css

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import customBarChart from "./components/customBarChart.js";
 import columnChart from "./components/columnChart.js";
 import groupedBarChart from "./components/groupedBarChart.js";
 import stackedColumnChart from "./components/stackedColumnChart.js";
+import setColors from "./lib/setColors.js";
+import setStyles from "./lib/setStyles.js";
+import sotaConfig from "./lib/sotaConfig.js";
 
 let sota = {};
 
@@ -17,12 +20,16 @@ sota.customBarChart = customBarChart;
 sota.columnChart = columnChart;
 sota.groupedBarChart = groupedBarChart;
 sota.stackedColumnChart = stackedColumnChart;
+sota.setColors = setColors;
+sota.setStyles = setStyles;
+sota.sotaConfig = sotaConfig;
+
+sota.setParam = function(prop, value){
+    this.sotaConfig[prop] = value;
+}
+
+sota.getParam = function(prop){
+    return this.sotaConfig[prop];
+}
 
 export default sota;
-
-/*
-TO COPY PASTE WHEN IMPORTING NEW COMPONENTS
-
-import boilerplate from "./components/boilerplate.js";
-sota.boilerplate = boilerplate;
-*/
